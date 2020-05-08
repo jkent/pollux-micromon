402c863d5f50f1f704feb35d0ec7d4ec18092cac
Jeff Kent <jeff@jkent.net>
2020-05-07 19:45:14 -0500

    default config handling

--- micromon/__init__.py
+++ micromon/__init__.py
@@ -1,8 +1,10 @@
 from os import path
+import shutil
 import subprocess
 import sys
 
 MICROMON_ROOT = path.normpath(path.join(path.dirname(path.abspath(__file__)), '..'))
+DEFAULT_CONFIG_PATH = path.normpath(path.join(MICROMON_ROOT, 'micromon.default.cfg'))
 CONFIG_PATH = path.normpath(path.join(MICROMON_ROOT, 'micromon.cfg'))
 MICROMON_BIN = path.normpath(path.join(MICROMON_ROOT, 'build', 'micromon.bin'))
 
@@ -13,6 +15,9 @@ if not path.exists(MICROMON_BIN):
     result = subprocess.check_call('make', cwd=MICROMON_ROOT)
     if result:
         sys.exit(result)
+
+if not path.exists(CONFIG_PATH):
+    shutil.copyfile(DEFAULT_CONFIG_PATH, CONFIG_PATH)
 
 from .core import Core
 from .target import Target
